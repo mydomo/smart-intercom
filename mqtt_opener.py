8d1fa991112457d594fc8b4b21872e7775848857
Marco Baglivo <marco.bag@gmail.com>
2020-01-01 18:55:29 +0100

    opener

--- mqtt_opener.py
+++ mqtt_opener.py
@@ -1,14 +1,23 @@
+#!/usr/bin/env python3
+
 import config
 from paho.mqtt.client import Client
+import RPi.GPIO as GPIO
+import time
 
 MQTT_USER = config.MQTT_USER
 MQTT_PASS = config.MQTT_PASS
+
 client = Client(client_id="citofono_1")
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26,GPIO.OUT)
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload.decode("utf-8")))
-
+    if str(msg.payload.decode("utf-8")) == "open":
+        GPIO.output(26,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(26,GPIO.LOW)
 
 client.on_message = on_message
 client.username_pw_set(username= MQTT_USER, password= MQTT_PASS)
